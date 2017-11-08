e626ffbad4ca953772bbcce799aaabf4c983f43c
jack01234 <1798204267@qq.com>
2017-11-08 22:50:16 +0800

    spring bean的自动装配

---
+++ spring_test/src/main/java/com/atguigu/spring/autowire/Car.java
@@ -0,0 +1,33 @@
+package com.atguigu.spring.autowire;
+
+/**
+ * Created by Administrator on 2017/11/8 0008.
+ */
+public class Car {
+    private String brand;
+    private double price;
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "brand='" + brand + '\'' +
+                ", price=" + price +
+                '}';
+    }
+}
